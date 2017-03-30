12492f3e8bd2c6bec697a4cb59d64448180bf295
taufan99 <atalarikt@gmail.com>
2017-03-30 21:41:10 +0700

    Groundbreaking commit (Taufan)                         Commit #1 (Taufan)                         Commit #2 (Taufan)                         Commit #3 (Taufan)                         Commit #4 (Taufan)                         Commit #5 (Taufan)                         Commit #6 (Taufan)

--- app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl22628/arika/MenuActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl22628/arika/MenuActivity.java
@@ -13,7 +13,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class Main2Activity extends AppCompatActivity
+public class MenuActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
@@ -80,17 +80,13 @@ public class Main2Activity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_search) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_status) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_account) {
 
         }
 
