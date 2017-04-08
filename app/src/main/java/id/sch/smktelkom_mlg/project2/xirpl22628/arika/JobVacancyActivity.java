1dc942caace540e4370d5a43ac1e10b13465775f
taufan99 <atalarikt@gmail.com>
2017-04-08 20:54:31 +0700

    Groundbreaking commit (Taufan)                         Commit #1 (Taufan)                         Commit #2 (Taufan)                         Commit #3 (Taufan)                         Commit #4 (Taufan)                         Commit #5 (Taufan)                         Commit #6 (Taufan)                         Commit #7 (Taufan)                         Commit #8 (Taufan)                         Commit #9 (Taufan)                         Commit #10 (Taufan)

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl22628/arika/JobVacancyActivity.java
@@ -0,0 +1,8 @@
+package id.sch.smktelkom_mlg.project2.xirpl22628.arika;
+
+/**
+ * Created by Mokleters on 4/8/2017.
+ */
+
+public class JobVacancyActivity {
+}
--- app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl22628/arika/User.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl22628/arika/User.java
@@ -14,10 +14,8 @@ public class User {
 
     // Default constructor required for calls to
     // DataSnapshot.getValue(User.class)
-    public User(String name, String email, String age) {
-    }
 
-    public User(String name, String email) {
+    public User(String name, String email, String age) {
         this.name = name;
         this.email = email;
         this.age = age;
