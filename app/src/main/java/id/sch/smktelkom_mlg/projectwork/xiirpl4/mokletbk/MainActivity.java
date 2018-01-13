171cafbb5194ea552336954f56501df70f736254
taufan99 <atalarikt@gmail.com>
2018-01-13 21:48:14 +0700

    Penambahan MainActivity dan penambahan radio button pada activity_main.

--- app/src/main/java/id/sch/smktelkom_mlg/projectwork/xiirpl4/mokletbk/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/projectwork/xiirpl4/mokletbk/MainActivity.java
@@ -1,10 +1,21 @@
 package id.sch.smktelkom_mlg.projectwork.xiirpl4.mokletbk;
 
 import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * Created by Mokleters on 1/12/2018.
  */
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+    private Button btnLapor, btnStat, signOut;
+    private ProgressBar progressBar;
+    private FirebaseAuth.AuthStateListener authListener;
+    private FirebaseAuth auth;
+
+
 }
